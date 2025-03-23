4460bc72feef5b52b63d35001da6da47768f8a70
Piyush_kr_Madhukar <105438331+Piyush5madhukar@users.noreply.github.com>
2025-03-23 19:40:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-# Load the trained LSTM model
+
 model = load_model("spam_classifier_lstm.h5")
 
-# Load the tokenizer
+
 with open("tokenizer.pkl", "rb") as file:
     tokenizer = pickle.load(file)
 
@@ -17,11 +17,10 @@ def classify_sms(text):
     prediction = (model.predict(padded_seq) > 0.5).astype("int32")
     return "Spam" if prediction[0][0] == 1 else "Ham"
 
-# Streamlit UI
 st.title("📩 SMS Spam Detector")
 st.write("Enter an SMS message below to check if it's Spam or Ham.")
 
-# Text input
+
 sms_text = st.text_area("Enter SMS Message:", "")
 
 if st.button("Classify SMS"):
@@ -34,4 +33,4 @@ if st.button("Classify SMS"):
     else:
         st.warning("Please enter a message to classify.")
 
-# Run using: streamlit run filename.py
+
